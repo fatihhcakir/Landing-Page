a4116669e52ac0873e2fcaecc3d29efed469f3bc
fatihhcakir <fatih.cakiir.0@gmail.com>
2024-07-13 17:29:17 +0300

    third commit

--- landing-page/src/Navbar.jsx
+++ landing-page/src/Navbar.jsx
@@ -1,8 +1,7 @@
-import React from 'react';
 
-const Header = () => {
+const Navbar = () => {
   return (
-    <header className="bg-gradient-to-r from-purple-500 to-pink-500 text-white">
+    <header className="bg-gradient-to-r from-indigo-300 to-purple-300 text-white">
       <div className="container mx-auto flex justify-between items-center p-4">
         <div className="flex items-center space-x-2">
           <img src="./logo.png" alt="Logo" className="h-8 w-8" />
@@ -11,20 +10,20 @@ const Header = () => {
 
         <nav className="flex-1">
           <ul className="flex justify-center space-x-4">
-            <li><a href="#useCase" className="hover:text-gray-400">Use Case</a></li>
-            <li><a href="#feature" className="hover:text-gray-400">Feature</a></li>
-            <li><a href="#pricing" className="hover:text-gray-400">Pricing</a></li>
-            <li><a href="#resources" className="hover:text-gray-400">Resources</a></li>
+            <li><a href="#useCase" className="font-bold text-black hover:text-gray-400">Use Case</a></li>
+            <li><a href="#feature" className="font-bold text-black hover:text-gray-400">Feature</a></li>
+            <li><a href="#pricing" className="font-bold text-black hover:text-gray-400">Pricing</a></li>
+            <li><a href="#resources" className="font-bold text-black hover:text-gray-400">Resources</a></li>
           </ul>
         </nav>
 
         <div className="flex items-center space-x-4">
-          <a href="#login" className="bg-indigo-600 text-white px-4 py-2 rounded-full text-sm font-medium hover:bg-indigo-700">Login</a>
-          <a href="#signup" className="bg-indigo-600 text-white px-4 py-2 rounded-full text-sm font-medium hover:bg-indigo-700">Signup</a>
+          <button className="bg-white text-black px-6 py-2 rounded-full text-sm font-medium hover:bg-indigo-400">Login</button>
+          <button className="bg-black text-white px-6 py-2 rounded-full text-sm font-medium hover:bg-indigo-400">Signup</button>
         </div>
       </div>
-    </header>
+    </header>   
   );
 };
 
-export default Header;
+export default Navbar;

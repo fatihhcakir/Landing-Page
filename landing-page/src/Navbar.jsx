4567cefe88c33e00b993153c4d4108ccad44659e
nisaltnbas <altnbasnisa@gmail.com>
2024-07-13 15:30:31 +0300

    tailwind css

--- landing-page/src/Navbar.jsx
+++ landing-page/src/Navbar.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import './App.css'; // Tailwind CSS sınıflarını içerecek şekilde
 
-function Navbar(){
-    return(
-        <h1>it goes here</h1>
-    )
-
+function Navbar() {
+  return (
+    <nav className="bg-gray-800 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-white text-lg font-bold">Logo</div>
+        <div className="flex space-x-4">
+          <a href="#" className="text-white">Link 1</a>
+          <a href="#" className="text-white">Link 2</a>
+          <a href="#" className="text-white">Link 3</a>
+          {/* Diğer bağlantılar */}
+        </div>
+        <div className="flex space-x-4">
+          <a href="#" className="text-white">Login</a>
+          <a href="#" className="text-white">Signup</a>
+        </div>
+      </div>
+    </nav>
+  );
 }
-export default Navbar
+
+export default Navbar;

c1f291af3bf94e151f387df7726ba9e7e7366dca
fatihhcakir <fatih.cakiir.0@gmail.com>
2024-07-13 15:48:35 +0300

    last

--- landing-page/src/Navbar.jsx
+++ landing-page/src/Navbar.jsx
@@ -4,22 +4,8 @@ import './App.css'; // Tailwind CSS sınıflarını içerecek şekilde
 
 function Navbar() {
   return (
-    <nav className="bg-gray-800 p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white text-lg font-bold">Logo</div>
-        <div className="flex space-x-4">
-          <a href="#" className="text-white">Link 1</a>
-          <a href="#" className="text-white">Link 2</a>
-          <a href="#" className="text-white">Link 3</a>
-          {/* Diğer bağlantılar */}
-        </div>
-        <div className="flex space-x-4">
-          <a href="#" className="text-white">Login</a>
-          <a href="#" className="text-white">Signup</a>
-        </div>
-      </div>
-    </nav>
-  );
+   <h1 className="text-sky-400"> selam</h1> 
+  )
 
 }
 

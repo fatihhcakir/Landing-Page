8b00febbe23452fb075efb6e3db3e6290e1eb58a
nisaltnbas <altnbasnisa@gmail.com>
2024-07-14 12:42:07 +0300

    how it works added

---
+++ landing-page/src/components/HowItWorks.jsx
@@ -0,0 +1,13 @@
+
+const HowItWorks = () => {
+  return (
+    <div className="p-4 border-b border-gray-200 justify-center ">
+      <h1 className="text-2xl font-bold ">How it works?</h1>
+      <p className="mt-2">
+        Video AI automatically selects, edits & captions top moments from your videos.
+      </p>
+    </div>
+  );
+};
+
+export default HowItWorks;

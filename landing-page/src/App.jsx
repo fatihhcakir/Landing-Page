c1d14bbf87861ecdb3be8900fbe9f0c4f8b5bcc8
Mertokai <merteyuboglu.16@hotmail.com>
2024-10-11 22:36:30 +0300

    commit2

--- landing-page/src/App.jsx
+++ landing-page/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
         <div className="absolute  md:top-16 top-[300px] left-1/2 transform -translate-x-1/2 w-2/3 mx-auto justify-center h-96 bg-gradient-to-r from-indigo-400 to-purple-400 rounded-md filter blur-3xl opacity-50 -z-50 flex flex-col min"></div>
         <Navbar />
         <Body />
+
         {/* <FooterMain /> */}
         {/* <HowItWorks/>
         <ThirdPage/> */}
